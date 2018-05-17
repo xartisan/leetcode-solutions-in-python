9bd6dd18d14eda3733e459a9b3b6da4ee88e1ad7
Vincent Wei <codeartisan@outlook.com>
2018-05-17 13:02:01 +0800

    Complete valid sudoku

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -3,5 +3,29 @@ class Solution:
         """
         :type board: List[List[str]]
         :rtype: bool
-        ""
-        
+        """
+        empty = '.'
+        row_collisions = set()
+        column_collisions = set()
+        sub_sudoku_collisions = set()
+
+        for row in range(9):
+            row_collisions.clear()
+            column_collisions.clear()
+            sub_sudoku_collisions.clear()
+            for col in range(9):
+                row_elem = board[row][col]
+                col_elem = board[col][row]
+                sub_sudoku_elem = board[row // 3 * 3 + col // 3][row % 3 * 3 + col % 3]
+
+                if row_elem in row_collisions or col_elem in column_collisions or \
+                    sub_sudoku_elem in sub_sudoku_collisions:
+                    return False
+                if row_elem != empty:
+                    row_collisions.add(row_elem)
+                if col_elem != empty:
+                    column_collisions.add(col_elem)
+                if sub_sudoku_elem != empty:
+                    sub_sudoku_collisions.add(sub_sudoku_elem)
+        return True
+
