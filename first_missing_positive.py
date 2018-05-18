20e9c7c2f740f47d797e8b2186a0e0d2e23b178a
Vincent Wei <codeartisan@outlook.com>
2018-05-18 08:46:11 +0800

    Complete first missing positive

--- first_missing_positive.py
+++ first_missing_positive.py
@@ -4,4 +4,19 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        
+        if not nums:
+            return 1
+        i = 0
+        exchange_count = 0
+        # O(n) guarantee
+        while i < len(nums) and exchange_count < len(nums):
+            cur_val = nums[i]
+            if cur_val < 0 or cur_val > len(nums) or cur_val == nums[cur_val - 1]:
+                i += 1
+            else:
+                nums[i], nums[cur_val - 1] = nums[cur_val - 1], cur_val
+                exchange_count += 1
+        for i, v in enumerate(nums, 1):
+            if i != v:
+                return i
+        return len(nums) + 1
