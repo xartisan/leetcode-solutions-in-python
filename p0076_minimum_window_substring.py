5f1c019d29f6a30303abcdfacd1965c0604a71cc
Vincent Wei <codeartisan@outlook.com>
2018-07-26 16:48:06 +0800

    Refactor p0076

--- p0076_minimum_window_substring.py
+++ p0076_minimum_window_substring.py
@@ -5,45 +5,42 @@ class Solution:
         :type t: str
         :rtype: str
         """
-        from collections import Counter
-        counter = Counter(t)
-        min_start = min_end = -1
-        remains = len(t)
-        more_chars = {}
-        cur_start = cur_end = -1
+
+        counter = {}
+        for c in t:
+            counter[c] = counter.setdefault(c, 0) + 1
+        remains = set(t)
+        left = -1
+        right = len(s)
+        cur_left = -1
 
         for i, c in enumerate(s):
-            v = counter.get(c)
-            if v is None:
+            if c not in counter:
                 continue
-            # c in target
-            # update start end
-            cur_end = i
-            if cur_start == -1:
-                cur_start = i
+            if cur_left == -1:
+                cur_left = i
 
-            if v != 0:
+            if c in remains:
                 counter[c] -= 1
-                remains -= 1
-                if remains == 0:
-                    min_start = cur_start
-                    min_end = i
+                if counter[c] == 0:
+                    remains.remove(c)
             else:
-                more_chars[c] = more_chars.setdefault(c, 0) + 1
-
-                if s[cur_start] == c:
-                    for l in range(cur_start, i + 1):
-                        char = s[l]
-                        if char not in counter:
-                            continue
-                        count = more_chars.get(char)
-                        if count is None or count == 0:
-                            break
-                        more_chars[char] -= 1
-
-                    cur_start = l
-                    if remains == 0 and cur_end - cur_start < min_end - min_start:
-                        min_start = cur_start
-                        min_end = cur_end
-            
-        return s[min_start:min_end + 1]
+                counter[c] -= 1
+                # shrink
+                next_cur_left = cur_left
+                while True:
+                    cc = s[next_cur_left]
+                    if cc not in counter:
+                        next_cur_left += 1
+                        continue
+                    if counter[cc] < 0:
+                        counter[cc] += 1
+                        next_cur_left += 1
+                    else:
+                        break
+                cur_left = next_cur_left
+            if not remains and i - cur_left < right - left:
+                left, right = cur_left, i
+        if left == -1:
+            return ""
+        return s[left: right + 1]
