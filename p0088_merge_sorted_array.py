339d1c90bf778200e1bef7a97ca1f2996e981c84
Vincent Wei <codeartisan@outlook.com>
2018-07-28 11:03:46 +0800

    Refactor p0088

--- p0088_merge_sorted_array.py
+++ p0088_merge_sorted_array.py
@@ -7,15 +7,15 @@ class Solution:
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
+        total_len = m + n
         m -= 1
         n -= 1
-        for i in range(len(nums1) - 1, -1, -1):
-            if n < 0:
+        for i in range(total_len - 1, -1, -1):
+            if n == -1:
                 return
-            
-            if m >= 0 and nums1[m] > nums2[n]:
-                nums1[i] = nums1[m]
-                m -= 1
-            else:
+            if m == -1 or nums1[m] <= nums2[n]:
                 nums1[i] = nums2[n]
                 n -= 1
+            else:
+                nums1[i] = nums1[m]
+                m -= 1
