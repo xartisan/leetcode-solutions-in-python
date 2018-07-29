7e3929a4b5bd0344f93373979c9d1acc4ae192a7
Vincent Wei <codeartisan@outlook.com>
2018-07-29 18:11:01 +0800

    Refactor p0098

--- p0098_validate_binary_search_tree.py
+++ p0098_validate_binary_search_tree.py
@@ -1,8 +1,10 @@
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
+
 
 class Solution:
     def isValidBST(self, root):
@@ -10,27 +12,14 @@ class Solution:
         :type root: TreeNode
         :rtype: bool
         """
-        def is_valid(l_bound, r_bound, node):
-            if node.left:
-                if node.left.val >= node.val:
-                    return False
-                if node.left.val <= l_bound:
-                    return False
-            if node.right:
-                if node.right.val <= node.val:
-                    return False
-                if node.right.val >= r_bound:
-                    return False
-            return True
-            
-        stack = []
-        stack.append((float('-inf'), float('inf'), root))
-        while stack:
-            l_bound, r_bound, node = stack.pop()
+
+        def is_valid_rec(node, left, right):
             if node is None:
-                continue
-            if not is_valid(l_bound, r_bound, node):
+                return True
+            v = node.val
+            if not left < v < right:
                 return False
-            stack.append((node.val, r_bound, node.right))
-            stack.append((l_bound, node.val, node.left))
-        return True
+
+            return is_valid_rec(node.left, left, v) and is_valid_rec(node.right, v, right)
+
+        return is_valid_rec(root, float('-inf'), float('inf'))
